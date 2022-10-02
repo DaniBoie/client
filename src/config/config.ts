435bdadbe46510c480b1f930bc54c375702bc25f
Daniel Ayala <dannyboy3385@gmail.com>
2022-10-02 08:56:34 -0700

    server listening on port 1337

---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+const config = {
+  firebase: {
+    apiKey: "AIzaSyD9W_cnZdXgxY04wic24SNaxsHFZvHIUQg",
+    authDomain: "ts-blog-dc46a.firebaseapp.com",
+    projectId: "ts-blog-dc46a",
+    storageBucket: "ts-blog-dc46a.appspot.com",
+    messagingSenderId: "570062620907",
+    appId: "1:570062620907:web:39409aac3bc7ba0eeb56ae"
+  },
+  server: {
+    url: 'http://localhost:1337'
+  }
+
+}
+
+export default config;
