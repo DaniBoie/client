3f41af3672d9f1b63eef3933619f0810070b0f92
Daniel Ayala <dannyboy3385@gmail.com>
2022-10-02 13:05:39 -0700

    Routes officially working for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import routes from './config/routes';
 
 export interface IApplicationProps {}
 
 const App: React.FunctionComponent<IApplicationProps> = (props) => {
-  return 0;
+  return (
+  
+    <Routes>
+      {routes.map((route, index) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            element={<route.component />}
+          />
+        )
+      })}
+    </Routes>
+
+  );
 };
 
 
